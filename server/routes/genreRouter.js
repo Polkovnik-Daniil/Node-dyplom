d90d81a34b00874a5826dba4182fac4e04ec1b6e
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-31 23:46:44 +0300

    updating routers

--- server/routes/genreRouter.js
+++ server/routes/genreRouter.js
@@ -5,14 +5,54 @@ const authMiddleware = require("../middleware/authMiddleware");
 const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 const checkLockedMiddleware = require("../middleware/checkLockedMiddleware");
 
-
-router.get("/pages/", authMiddleware, checkRoleMiddleware("Admin, Moderator"), checkLockedMiddleware, genreController.getCountPages);
-router.get("/page/:id", authMiddleware, checkRoleMiddleware("Admin, Moderator"), checkLockedMiddleware, genreController.getPage); 
-router.get("/element/:id", authMiddleware, checkRoleMiddleware("Admin, Moderator"), checkLockedMiddleware, genreController.getById); //будет ли известен id пользователю??
-router.post("/create/", authMiddleware, checkRoleMiddleware("Admin, Moderator"), checkLockedMiddleware, genreController.create);
-router.post("/create-array/", authMiddleware, checkRoleMiddleware("Admin, Moderator"), checkLockedMiddleware, genreController.createArray); //it is necessary to check the operability
-router.put("/update/:id", authMiddleware, checkRoleMiddleware("Admin, Moderator"), checkLockedMiddleware, genreController.updateElement); //на тот случай если id не будет известен
-router.put("/update/:id", authMiddleware, checkRoleMiddleware("Admin, Moderator"), checkLockedMiddleware, genreController.updateElement); //будет ли известен id пользователю??
-
+router.get(
+  "/pages/",
+  authMiddleware,
+  checkRoleMiddleware("Admin, Moderator"),
+  checkLockedMiddleware,
+  genreController.getCountPages
+);
+router.get(
+  "/page/:id",
+  authMiddleware,
+  checkRoleMiddleware("Admin, Moderator"),
+  checkLockedMiddleware,
+  genreController.getPage
+);
+router.get(
+  "/element/:id",
+  authMiddleware,
+  checkRoleMiddleware("Admin, Moderator"),
+  checkLockedMiddleware,
+  genreController.getById
+); //будет ли известен id пользователю??
+router.post(
+  "/create/",
+  authMiddleware,
+  checkRoleMiddleware("Admin, Moderator"),
+  checkLockedMiddleware,
+  genreController.create
+);
+router.post(
+  "/create-array/",
+  authMiddleware,
+  checkRoleMiddleware("Admin, Moderator"),
+  checkLockedMiddleware,
+  genreController.createArray
+); //it is necessary to check the operability
+router.put(
+  "/update/:id",
+  authMiddleware,
+  checkRoleMiddleware("Admin, Moderator"),
+  checkLockedMiddleware,
+  genreController.updateElement
+); //на тот случай если id не будет известен
+router.put(
+  "/update/:id",
+  authMiddleware,
+  checkRoleMiddleware("Admin, Moderator"),
+  checkLockedMiddleware,
+  genreController.updateElement
+); //будет ли известен id пользователю??
 
 module.exports = router;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,12 +2,19 @@ const Router = require("express");
 const router = new Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
-const checkRole = require("../middleware/checkRoleMiddleware");
+const checkLockedMiddleware = require("../middleware/checkLockedMiddleware");
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 
 router.post("/registration", userController.registration);
 router.post("/login", userController.login);
 router.get("/auth", authMiddleware, userController.check);
 //Обработчик отвечающий за провекру роли
-router.get("/element/:id", checkRole("Admin"), userController.getById); //будет ли известен id пользователю??
+router.get(
+  "/element/:id",
+  authMiddleware,
+  checkRoleMiddleware("Admin"),
+  checkLockedMiddleware,
+  userController.getById
+); //будет ли известен id пользователю??
 
 module.exports = router;
